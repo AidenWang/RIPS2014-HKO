692b408c612d227b091cb68121d6d9b8077d71e3
helsachan <helsachan@gmail.com>
2014-07-24 16:32:30 +0800

    Update CuckooSearch_forSWIRLS_multiWithMP.py

--- CuckooSearch_forSWIRLS_multiWithMP.py
+++ CuckooSearch_forSWIRLS_multiWithMP.py
@@ -1,7 +1,6 @@
 #!C:\Python27\ScriptDump 
 #Cuckoo Search Algorithm 
-
-import main
+  
 #Convert string to a number 
 def num(s): 
     import exceptions 
@@ -11,39 +10,17 @@ def num(s):
         return float(s) 
   
 #input is a list of 6 parameters and outputs the fitness of the solution 
-
-
  
-def get_fitness(a_nest): 
-    
+def get_fitness(a_nest, settings): 
     import math 
     import get_score 
-    
- 
-    temp_nest = a_nest
-    nd = len(temp_nest[0])
-    #print 'length temp', len(temp_nest)
-    for j in range(nd):
-       
-	if j==1 or j==0:
-    		temp_nest[0][j] = int(round(temp_nest[0][j])) 
- 
-        elif j==5:
-               
-                temp_nest[0][j] = int(round(temp_nest[0][j]/float(6))*6) 
-                
+    a_nest[0] = int(round(a_nest[0])) 
+    a_nest[1] = int(round(a_nest[1]))
+    a_nest[2] = int(round(a_nest[2]))
+    a_nest[5] = int(round(a_nest[5]/float(6))*6) 
     #Calls SWIRLS and scoring function 
-    
-    dump = get_score.get_score(temp_nest[0], main.settings)
-    
-    return dump
-
-def add_settings(a_nest, settings):
-    temp_nest = [0]*2
-    temp_nest[0] = a_nest
-    temp_nest[1] = settings
-    return temp_nest
-
+    a_nest[6] = get_score.get_score(a_nest[:6],settings) 
+    return a_nest[6]
 
 def leapyr(n):
     if n % 400 == 0:
@@ -54,7 +31,6 @@ def leapyr(n):
         return True
     else:
         return False
-
 
 def change_settings(settings): #base time shifting
     timestr = str(settings[0])
@@ -90,7 +66,6 @@ def change_settings(settings): #base time shifting
     if mm == 1 and mm != tmpm:
         yy = yy + 1
 
-
     yystr = str(yy)
     if mm < 10:
         mmstr = '0' + str(mm)
@@ -112,100 +87,72 @@ def change_settings(settings): #base time shifting
     settings[0] = yystr + mmstr + ddstr + hhstr + nnstr
     
     return settings[0]
-        
-  
+
+
 #input new and old solutions with new and old scores and replaces old if new has higher score 
 def replace_nests(nests, new_nests, settings):
-    import numpy, multiprocessing
+    import numpy
     import time
     from operator import itemgetter  
-    pool = multiprocessing.Pool()
 
-    print '\n'*3 + 'Producing new nests for ' + str(settings[0]) + ':' + '\n'
+    print 'Producing new scores for ' + str(settings[0]) + ':' + '\n'
     
     """
     print '\n' + '='*20 + ' REPLACING NESTS ' + '='*20 + '\n'
     for i in range(len(nests)):
         print ' '*(3-len(str(i+1))), str(i+1) , '   ' , str(nests[i])"""
 
-
     start_time = time.time()
-
 
     print 'Old nests: \n'
 
-    score_new_nests = [0]*32
-    
-    for i in range(32):
-         score_new_nests[i] = add_settings(nests[i], settings)
-
-    new_fitness = pool.map(get_fitness, score_new_nests)
-
     for i in range(len(nests)):
-    	#get_fitness(nests[i])
+    	nests[i][6] = get_fitness(nests[i], settings)
 	if i*100/(len(nests))/10 < (i+1)*100/(len(nests))/10:
 	    print str(i*100/(len(nests)))+ '% done'
 
-
     print '\n' + '='*17 + ' FITNESS OF NEW NESTS ' + '='*17 + '\n'
-    
-
-
-    score_new_nests = [0]*len(new_nests)
 
     for i in range(len(new_nests)):
-         score_new_nests[i] = add_settings(new_nests[i], settings)
-
-    new_fitness = pool.map(get_fitness, score_new_nests)
-
-    for i in range(len(new_nests)):
-        #get_fitness(new_nests[i])
-        new_nests[i][2] = int(round(new_nests[i][2]))
+        new_nests[i][6] = get_fitness(new_nests[i], settings)
 	print 'The fitness of', new_nests[i][:6], 'is', new_nests[i][6]
-
 
     #if numpy.std(nests[:][6]) < 10000: #do something when the scores get too close
 	#print 'Too close!'
 
-
     nests = nests + new_nests
 
-
     nests.sort(key=lambda x: x[6], reverse = True)
-
 
     """
     print '\n' + '='*20 + ' SORTED NESTS ' + '='*20 + '\n'
 
-
     for i in range(len(nests)):
         print ' '*(3-len(str(i+1))), str(i+1) , '   ' , str(nests[i])"""
 
-
     for i in range(len(new_nests)): 
-        nests.pop()
-
+	nests.pop()
 
     print '\n' + '='*20 + ' UPDATED NESTS ' + '='*20 + '\n'
-
 
     for i in range(len(nests)):
         print ' '*(3-len(str(i+1))), str(i+1) , '   ' , str(nests[i])
 
+    #seperates settings sent from main
+    date_list = settings[0]; base_dir = settings[1]; 
+    save_dir = settings[2]; save_as = settings[3]
+
+    #Saves set of parameters with best scores to save_dir/save_as_best_score
 
     end_time = time.time()
 
-
     print '\n'+'Time spent: ' + str(end_time - start_time) + ' seconds' + '\n'
-
 
     """  
     print '\n' + '='*20 + ' LIST OF NEW NESTS ' + '='*20 + '\n'
 
-
     for i in range(len(new_nests)):
         print ' '*(3-len(str(i+1))), str(i+1) , '   ' , str(new_nests[i])"""
-
 
     return nests
     
@@ -214,7 +161,7 @@ def replace_nests(nests, new_nests, settings):
 def get_new_nests(nests, Lb, Ub, nest_number, stepsize, percentage): 
     import math 
     import scipy.special 
-    #import levy
+    import levy
     import random
     from operator import itemgetter 
     #Mantegna's Algorithm 
@@ -224,10 +171,10 @@ def get_new_nests(nests, Lb, Ub, nest_number, stepsize, percentage):
     nests.sort(key=lambda x: x[6], reverse = True) #arranging the nests in descending order of scores
     best_nest = nests[0]
 
-
     frac = int(round(nest_number*percentage))
     random.shuffle(nests[int((nest_number-frac)*0.5):])
     new_nests = [[0 for i in range(7)] for j in range(frac)]
+    #new_nests = [[0 for i in range(7)] for j in range(nest_number)]
     for i in range(frac): 
         temp = nests[nest_number-i-1][:] #so temp saves the worst "frac" number of nests
         step = [0]*len(temp) 
@@ -312,11 +259,15 @@ def empty_nest(nests,Lb,Ub,pa,nest_number,nd,fitness,settings):
   
 #The function that calls other functions for cuckoo search. 
 def cuckoo_search(nests, Iterations, nest_number, settings): 
+    import moving_max, get_data
     import random 
-    import math, multiprocessing 
+    import math, multiprocessing, time
     import os 
     import exceptions 
     import string 
+    import main
+    import copy
+    
     #These are all changable parameters 
     nd = 7 #dimension of solutions 
     Lb = [0,0,1,1,1,6,0]  #lower bound to search domain. Same dimension of solutions 
@@ -324,84 +275,176 @@ def cuckoo_search(nests, Iterations, nest_number, settings):
     stepsize = [1,1,0.75,0.75,0.75,1.5,0] #new addition 7/22 
     pa = 0.2 #the percent of nests(solutions) that are replaced per step 
     
+    #initiates number by parameters in the sort file 
+    #Initates nest_number of solutions by randomly picking points in parameter space 
+    #for i in range(nest_number): 
+    #    a_nest = [0]*nd 
+    #   for j in range(nd): 
+    #        #!!!The second parameter must be >= first parameter so the following line guarantees this. 
+    #            #!!!Change or comment out if different conditions are necessary 
+    #        if j == 1: 
+    #                a_nest[j] = round((Ub[j]-a_nest[0])*random.random() + a_nest[0],3) 
+    #            else: 
+    #                #Picks random points in parameter space 
+    #                a_nest[j] = round(((Ub[j]-Lb[j])*random.random()) + Lb[j],3) 
+    #    nests[i] = a_nest 
+    #Opens parallel processing 
+    #N_inter = 0
 
     change_settings(settings)
-    nests = replace_nests(nests, get_new_nests(nests, Lb, Ub, nest_number, stepsize, pa), settings)
-    print settings[0]
+    print '\n'*3 
 
+    #Generate actual raingauge list 
+    actual = get_data.get_actual(settings[0], settings[1])
+
+    #Actual peak finding
+    #actual_maxes = moving_max.moving_max(actual)
+
+    print 'Actual maxes for ' + str(settings[0]) + ': ' 
+    print actual, '\n'
+    #print actual_maxes, '\n'
+
+    #only do Levy flights if the rainfall amount is of importance
+    if sum(actual)/len(actual) > 2:
+        nests = replace_nests(nests, get_new_nests(nests, Lb, Ub, nest_number, stepsize, pa), settings)
+
+    else:
+        print "No tuning involved! Reusing the old nests: \n"
+        start_time = time.time()
+
+        for i in range(nest_number):
+            nests[i][6] = 0.0
+        print '\n'
+
+        for i in range(nest_number):
+            print ' '*(3-len(str(i+1))), str(i+1) , '   ' , str(nests[i])
+
+        print '\n'
+
+        end_time = time.time()
+
+        print '\n'+'Time spent: ' + str(end_time - start_time) + ' seconds' + '\n'
+
+    return nests
+        
+    #print settings[0]
 
     """print 'NOW THE 32 PARAMETERS ARE:' + '\n'
     for i in range(len(nests)):
          print ' '*(3-len(str(i+1))), str(i+1) , '   ' , str(nests[i])"""
       
       
+    #    new_nests = get_cuckoo(nests,best_nest,Lb,Ub, nest_number,nd,stepsize) 
+    #    N_inter = N_inter+1
+  
+    #    score_new_nests = [0]*nest_number 
+    #    for j in range(nest_number): 
     
-
+    #        score_new_nests[j] = add_settings(new_nests[j], settings) 
+    #    new_fitness = pool.map(get_fitness,score_new_nests) 
+          
+    #    data_dump = replace_nests(nests,new_nests,nest_number,nd,fitness,new_fitness) 
+    #    nests = data_dump[0] 
+    #    fitness = data_dump[1] 
+          
+        #and therefore should follow any new_nest function 
+    #    data_dump = empty_nest(nests,Lb,Ub,pa,nest_number,nd,fitness,settings) 
+    #    nests = data_dump[0] 
+    #    fitness = data_dump[1] 
+    #    index = fitness.index(max(fitness)) 
+    #    best_nest = nests[index][:] 
+    #    best_nest_print = best_nest[:] 
+        
+    #    for j in range(nd): 
+    #        if j == 0 or j == 1: 
+    #            best_nest_print[j] = int(round(best_nest_print[j])) 
+    #        elif j == 5: 
+    #            best_nest_print[j] = int(round((best_nest_print[j]/6))*6) 
+          
+        #percent = float(N_inter)/Iterations 
+        #print 'The Percent Done = ' + str(percent) 
+        #int(N_inter) 
+        #print 'The Best Solution is ' + str(best_nest_print) 
+    #return best_nest[:] 
 
 #The function that calls other functions for cuckoo search. 
 def initialize(Iterations, nest_number, settings): 
+    import time
     import random 
     import math, multiprocessing 
     import os 
     import exceptions 
     import string 
+    import generate
+    import get_data
     
     #These are all changable parameters 
     nd = 7 #dimension of solutions 
     Lb = [0,0,1,1,1,6,0]  #lower bound to search domain. Same dimension of solutions 
     Ub = [7,7,24,50000,9,60,1]  #upper bound to search domain. Same dimension of solutions 
     stepsize = [1,1,0.75,0.75,0.75,1.5,0] #new addition 7/22 
-    pa = 0.2 #the percent of nests(solutions) that are replaced per step 
-    nests = [[0] for i in range(nest_number)]
-    nests[0] = [1, 7, 9, 2000, 1.5, 12, 0]
-    nests[1] = [1, 7, 9, 2000, 1.5, 30, 0]
-    nests[2] = [1, 7, 9, 2000, 2.5, 12, 0]
-    nests[3] = [1, 7, 9, 2000, 2.5, 30, 0]
-    nests[4] = [1, 7, 9, 2000, 3, 6, 0]
-    nests[5] = [1, 7, 9, 2000, 3, 12, 0]
-    nests[6] = [1, 7, 9, 2000, 3, 30, 0]
-    nests[7] = [1, 7, 9, 10000, 1.5, 6, 0]
-    nests[8] = [1, 7, 9, 10000, 1.5, 12, 0]
-    nests[9] = [1, 7, 9, 10000, 1.5, 30, 0]
-    nests[10] = [1, 7, 9, 10000, 2.5, 6, 0]
-    nests[11] = [1, 7, 9, 10000, 2.5, 12, 0]
-    nests[12] = [1, 7, 9, 10000, 2.5, 30, 0]
-    nests[13] = [1, 7, 9, 10000, 3, 6, 0]
-    nests[14] = [1, 7, 9, 10000, 3, 12, 0]
-    nests[15] = [1, 7, 9, 10000, 3, 30, 0]
-    nests[16] = [2, 7, 9, 2000, 1.5, 6, 0]
-    nests[17] = [2, 7, 9, 2000, 1.5, 12, 0]
-    nests[18] = [2, 7, 9, 2000, 1.5, 30, 0]
-    nests[19] = [2, 7, 9, 2000, 2.5, 30, 0]
-    nests[20] = [2, 7, 9, 2000, 3, 6, 0]
-    nests[21] = [2, 7, 9, 2000, 3, 12, 0]
-    nests[22] = [2, 7, 9, 2000, 3, 30, 0]
-    nests[23] = [2, 7, 9, 10000, 1.5, 6, 0]
-    nests[24] = [2, 7, 9, 10000, 1.5, 12, 0]
-    nests[25] = [2, 7, 9, 10000, 1.5, 30, 0]
-    nests[26] = [2, 7, 9, 10000, 2.5, 6, 0]
-    nests[27] = [2, 7, 9, 10000, 2.5, 12, 0]
-    nests[28] = [2, 7, 9, 10000, 2.5, 30, 0]
-    nests[29] = [2, 7, 9, 10000, 3, 6, 0]
-    nests[30] = [2, 7, 9, 10000, 3, 12, 0]
-    nests[31] = [2, 7, 9, 10000, 3, 30, 0]
 
-   
-    pool = multiprocessing.Pool()
+    nest_list = [[[0,0], [0], [0], [0, 0], [0, 0, 0], [0, 0, 0]] for i in range(100)]
+    nest_list[0] = [[1, 2], [7], [9], [2000, 10000], [1.5, 2.5, 3], [6, 12, 30]] #initial
+    nest_list[1] = [[2, 3], [7], [9], [2000, 10000], [2, 2.8, 3], [6, 30, 60]]
 
-    print '\n'*3 + '='*20 + ' INITIALIZING ' + '='*20 + '\n'
-   
-    score_new_nests = [0]*nest_number
+    nests = generate.generate([[1, 2], [7], [9], [2000, 10000], [1.5, 2.5, 3], [6, 12, 30]]) #put your settings here
+
+    #Opens parallel processing 
+    #Opens parallel processing
+    #score_nests = [0]*nest_number 
+    #sends jobs to get_fitness in parallel
+    #fitness = pool.map(get_fitness,score_nests) 
+    #I don't know multi-processing, please help if you have any idea ----Aiden
+
+    pool = multiprocessing.Pool() 
+
+    #Generate actual raingauge list 
+    actual = get_data.get_actual(settings[0], settings[1])
+
+    #Actual peak finding
+    #actual_maxes = moving_max.moving_max(actual)
+
+    print '\n'*3
+
+    print 'Actual maxes for ' + str(settings[0]) + ': ' 
+    print actual, '\n'
+    #print actual_maxes, '\n'
+
+
+    print '='*20 + ' INITIALIZING ' + '='*20 + '\n'
+
+    #initiates number by parameters in the sort file 
+    #Initates nest_number of solutions by randomly picking points in parameter space 
+    #for i in range(nest_number): 
+    #    a_nest = [0]*nd 
+    #   for j in range(nd): 
+    #        #!!!The second parameter must be >= first parameter so the following line guarantees this. 
+    #            #!!!Change or comment out if different conditions are necessary 
+    #        if j == 1: 
+    #                a_nest[j] = round((Ub[j]-a_nest[0])*random.random() + a_nest[0],3) 
+    #            else: 
+    #                #Picks random points in parameter space 
+    #                a_nest[j] = round(((Ub[j]-Lb[j])*random.random()) + Lb[j],3) 
+    #    nests[i] = a_nest 
     
+    start_time = time.time()
+  
+
     for i in range(nest_number):
-         score_new_nests[i] = add_settings(nests[i], settings)
-         
-    
-    new_fitness = pool.map(get_fitness, score_new_nests)
-    
+        nests[i][6] = get_fitness(nests[i],settings)
+	if i*100/(nest_number)/10 < (i+1)*100/(len(nests))/10:
+	    print str(i*100/(len(nests)))+ '% done'
+
+
+
+
+    nests.sort(key=lambda x: x[6], reverse = True)
+
     for i in range(nest_number):
-         nests[i][6] = new_fitness[i]
-         print 'The fitness of', nests[i][6], 'is', nests[i][6]
+	print 'The fitness of', nests[i][:6], 'is', nests[i][6]
+
+    end_time = time.time()
+    print '\n'+'Time spent for initialization: ' + str(end_time - start_time) + ' seconds' + '\n'
         
     return nests
-
