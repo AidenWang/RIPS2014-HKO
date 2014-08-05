665fe976635dcb367f5f41a43ca40e589a1ecbe3
helsachan <helsachan@gmail.com>
2014-08-05 18:47:40 +0800

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,13 +1,29 @@
+############################################################################
+                              
+                                #get_data.py
+                     #Returns the forecasted and actual data
+
+############################################################################
+
 import os, datetime, time, sys, random
 
-def get_forecast(date_list, min_lvl, max_lvl, rho, alpha, sigma, interval, base_dir, save_dir):
-    # Runs the integrated script for SWIRLS forecaset, converts to list
+############################################################################
+############################################################################
+
+#Returns the forecasted data for the given data
+def get_forecast(date_list, min_lvl, max_lvl, rho, alpha, sigma, interval,
+    base_dir, save_dir):
+    import Cowbird
+
+    #Runs the integrated script for SWIRLS forecast and converts to a list
     timestr = str(date_list)
-    # converts time to datetime format for addition of times
+
+    #Converts time to datetime format for addition of times
     yy = int(timestr[0:4]); mm = int(timestr[4:6]); dd = int(timestr[6:8])
     hh = int(timestr[8:10]); nn = int(timestr[10:12])
     startdatetime = datetime.datetime(yy,mm,dd,hh,nn)
-    # folder for forecast data
+
+    #Folder for forecast data
     forecastpath = base_dir + "data/archive/" + str(yy) + \
         "/" + str(yy) + str(mm).zfill(2) + "/" + str(yy) + \
         str(mm).zfill(2) + str(dd).zfill(2) + "/output/statistic/" + \
@@ -18,30 +34,37 @@ def get_forecast(date_list, min_lvl, max_lvl, rho, alpha, sigma, interval, base_
     params = str(min_lvl) + " " + str(max_lvl) + " " + str(rho) + " "\
         + str(alpha) +  " " + str(sigma) + " " + str(interval)
 
-    ## print '\n', 'Go to compare ' + timestr
-    # checks if forecast has already been run
+    #Checks if forecast has already been run
     while not os.path.isfile(forecastpath + '/ts_rank14p_' + timestr):
-        #if it hasn't run it
+
+        #If it hasn't been run
 	os.chdir(base_dir + "spt")
-	os.system('./ForTuningCycle.spt ' + timestr + ' ' + params + ' > ' + \
-	'/dev/null 2> /dev/null')
-        #if the file still doesn't exist, output all zero forecast to guarantee 0 score
+	os.system('./ForTuningCycle.spt ' + timestr + ' ' + params + ' > '
+            + '/dev/null 2> /dev/null')
+
+        #If the file still doesn't exist, output all zero forecast to 
+        #guarantee 0 score
         if not os.path.isfile(forecastpath + '/ts_rank14p_' + timestr):
-            #print/save error
+
+            #Print and save error
             print '\n', '\n', '\n', 'ERROR!!!!!!'
-            print '\n', 'Missing data ' + forecastpath + '/ts_rank14p_' + timestr
+            print '\n', 'Missing data ' + forecastpath + '/ts_rank14p_' + \
+                  timestr
             print params
             f = open(save_dir + 'Bad_Parameters', 'a')
             f.write('['+str(min_lvl)+', '+str(max_lvl)+', '+str(rho)+', '+ \
                 str(alpha)+', '+str(sigma)+', '+str(interval)+']\n')
             f.close()
             return [0]*60
-    # Parses forecast, creates list
+
+    #Parses forecast, creates list
     os.chdir(forecastpath)
-    # list for forecast data
+
+    #List for forecast data
     forecast = []
     titleline = 1
-    # adds data to list
+
+    #Adds data to list
     forecastfile = open("ts_rank14p_" + timestr)
     for line in forecastfile:
         if titleline == 1:
@@ -50,31 +73,37 @@ def get_forecast(date_list, min_lvl, max_lvl, rho, alpha, sigma, interval, base_
         contents = line.split()
         forecast.append(float(contents[1]))
     forecastfile.close()
-    # deletes raw output that takes up lots of space
+
+    #Deletes raw output that takes up lots of space
     os.chdir(base_dir + "data/archive/" + str(yy) + "/" + str(yy) + \
         str(mm).zfill(2) + "/" + str(yy) + str(mm).zfill(2) + \
         str(dd).zfill(2) + "/output_raw/irregular/qpf/2/064/")
-    os.system("rm -rf mugof-lv" + str(min_lvl) + "-max" + str(max_lvl) + "-r" + str(rho) + \
-        "-a" + str(alpha) + "-s" + str(sigma) + "-i" + str(interval) + "-dbz33_zrhk")
-    # Returns forecast
+    os.system("rm -rf mugof-lv" + str(min_lvl) + "-max" + str(max_lvl) + \
+        "-r" + str(rho) + "-a" + str(alpha) + "-s" + str(sigma) + "-i" + \
+        str(interval) + "-dbz33_zrhk")
+
+    #Returns forecast
     return forecast
     
+############################################################################
+############################################################################
 
-
-
-
-
+#Runs actual data preparation script
 def prepare_actual(input_list):
-    # Runs actual data preparation script
+
+    #Splits up given settings
     intime = input_list[0]; base_dir = input_list[1]
-    # parses input
+
+    #Parses input
     timestr = str(intime)
     yy = int(timestr[0:4]); mm = int(timestr[4:6]); dd = int(timestr[6:8])
     hh = int(timestr[8:10]); nn = int(timestr[10:12])
     startdatetime = datetime.datetime(yy,mm,dd,hh,nn)
-    # loops through all times in forecast range
+
+    #Loops through all times in forecast range
     for t in range(0, 120, 5):
-        # runs data preparation script at each available interval in forecast (5 min)
+
+        #Runs data preparation script at each available interval in forecast
         tempdatetime = startdatetime + datetime.timedelta(0,0,0,0,t)
         temptuple = tempdatetime.timetuple()
         temptimestr = time.strftime("%Y%m%d%H%M", temptuple)
@@ -82,33 +111,37 @@ def prepare_actual(input_list):
         os.system("./ForPreparation.spt " + temptimestr + " > " + \
             '/dev/null')            
         os.chdir('../module/04_post-processing/12_make_obs_rank_db_60qpfAtGauge/spt')
-        os.system('./MakeObsRank.spt ' + str(temptimestr) + ' ' + str(temptimestr) + \
-            ' 5 14 dummy > ' + '/dev/null')
+        os.system('./MakeObsRank.spt ' + str(temptimestr) + ' ' + \
+            str(temptimestr) + ' 5 14 dummy > ' + '/dev/null')
+
+############################################################################
+############################################################################
 
 
-
-
-
-
+#Returns the actual data for the given data
 def get_actual(intime, base_dir):
     import glob
-    # Parses actual data to list
-    # parses input
+    #Parses actual data to list
     timestr = str(intime)
     yy = int(timestr[0:4]); mm = int(timestr[4:6]); dd = int(timestr[6:8])
     hh = int(timestr[8:10]); nn = int(timestr[10:12])
     startdatetime = datetime.datetime(yy,mm,dd,hh,nn)
-    # list for holding actual raingauge data
-    raingauge = []
 
+    #List for holding actual raingauge data
+    raingauge = []
     for t in range(0, 360, 5):
-        # figures out time info for temptime
+
+        #Determines time info for temptime
         tempdatetime = startdatetime + datetime.timedelta(0,0,0,0,t)
         temptuple = tempdatetime.timetuple()
         temptimestr = time.strftime("%Y%m%d%H%M", temptuple)
-        tyy = int(temptimestr[0:4]); tmm = int(temptimestr[4:6]); tdd = int(temptimestr[6:8])
-        thh = int(temptimestr[8:10]); tnn = int(temptimestr[10:12])
-        # parses data, adds rainfall intensity to rainguage list
+        tyy = int(temptimestr[0:4])
+        tmm = int(temptimestr[4:6]) 
+        tdd = int(temptimestr[6:8])
+        thh = int(temptimestr[8:10])
+        tnn = int(temptimestr[10:12])
+
+        #Parses data and adds rainfall intensity to rainguage list
         raingaugepath = base_dir + "data/archive/" + \
             str(tyy) + "/" + str(tyy) + str(tmm).zfill(2) + "/" + str(tyy) + \
             str(tmm).zfill(2) + str(tdd).zfill(2) + "/output/statistic/" + \
@@ -116,10 +149,14 @@ def get_actual(intime, base_dir):
         os.chdir(raingaugepath)
         titleline = 1
 
+        #Grabs all the names of the files in a particular folder to form a list
         test_temptimestr = glob.glob("rank14p_*") 
 
+        #Check if the data for a particular date in time is missing
         if "rank14p_" + temptimestr in test_temptimestr:
             raingaugefile = open("rank14p_" + temptimestr)
+        #If data missing, grab the previous data from last date
+        #NOTE: Does not work for two or more consecutive dates of missing data
         else: 
             tempdatetime2 = startdatetime + datetime.timedelta(0,0,0,0,t-5)
             temptuple2 = tempdatetime2.timetuple()
@@ -132,5 +169,7 @@ def get_actual(intime, base_dir):
             contents = line.split()
             raingauge.append(float(contents[1]))
         raingaugefile.close()
-    # Returns list of actual rainfall intensity
+
+    #Returns list of actual rainfall intensity
     return raingauge
+
