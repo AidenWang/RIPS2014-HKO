76d1c8d037c9e2ff7dc02c4a203eb8fe48384082
helsachan <helsachan@gmail.com>
2014-08-05 18:47:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,24 @@
 
 
 #Date and time to start forecast in YYYYMMDDHHNN format 
-#date_list = [200806070500, 200906110800, 201006100300, 201007281300, \
-             #201204290100]
-#end_list = [200806071300, 200906111600, 201006101100, 201007282100, \
-            #201204290900]
-date_list = [200906110800, 201006100300, 201007281300]
-end_list = [200906111600, 201006101100, 201007282100]
+
+#Date type:
+#1-Single base time, runs continuously until running out of data
+#2-Multiple ranges of dates, runs until finishes forecast for all times
+#  within the ranges
+
+date_type = 1
+
+if date_type == 1:
+   date_list = [201204291000] #input your base time here
+
+if date_type == 2:
+   date_list = [201209240000] #input your starting times here
+   end_list = [201209242355] #input your ending times here
+
+"""date_list = [200906110800, 201006100300, 201007281300, 201204290100]
+end_list = [200906111600, 201006101100, 201007282100, 201204290900]"""
+
 #Additional date lists
 #date_list = [201005070400, 200904250800, 200903240700, 201104171530]  
 #date_list = [201005070400, 200904250800, 200903240700, 201104171530] #squalls 
@@ -56,8 +68,11 @@ timing = []
 #Tracks how many tuning runs have occured
 runs = 0
 
-#List of settings to be  passed 
-settings = [date_list, base_dir, save_dir, save_as] 
+#List of settings to be passed 
+if date_type == 1:
+    settings = [date_list, base_dir, save_dir, save_as] 
+if date_type == 2:
+    settings = [date_list, base_dir, save_dir, save_as] 
 
 #Accessors for prepare.py 
 #Do not use accessors for any routines run in parallel! 
@@ -70,80 +85,163 @@ def get_save_dir():
 def get_save_as():
     return save_as
 
+#Stores original save_as name 
+original_save_as = save_as 
+
 #This block run when main.py is called directly from the terminal
 if __name__ == "__main__": 
     import GS, psof, sort_file, time
     import Cowbird, numpy
 
-    #Run if Cowbird chosen as the preferred algorithm
-    if optimization_type == 1:
+    #single date
 
-          #Run Grasshopper for all listed dates
-          for i in range(len(date_list)):
-               print i, 'at the beginning'
-               end_list[i], 'at the beginning'
+    if date_type == 1:
 
-               #Stores original save_as name 
-               original_save_as = save_as 
+        if optimization_type == 1:
 
-               #For date in date_list: 
-               #Appends the date to the end of original save_as name 
-               save_as = original_save_as + str(date_list[i]) 
+            #appends the date to the end of original save_as name 
+            save_as = original_save_as + str(date_list[0]) 
+            #changes the settings passed to get_score to only include a single date 
+            settings = [date_list[0], base_dir, save_dir, save_as] 
 
-               #Changes the settings passed to get_score to only include a 
-               #single date 
-               settings = [date_list[i], base_dir, save_dir, save_as] 
+            #runs desired optimization 
+            print '\n'*3 + 'Input date list: ' + str(date_list[0])
 
-               #Runs desired optimization 
-               print '\n'*3 + 'Input date list: ' + str(date_list)
+	    nests = Cowbird.initialize(iterations, solution_number, settings)
 
-               #Begin timing initialization and add to vector "timing"
-               timing.append(time.time())
+            #Printing nests list into file
+            b = open(save_dir + 'current_nest_list.txt', 'w') 
+            b.write('')
+            b = open(save_dir + 'current_nest_list.txt', 'a')
+            b.write('Time: ' + date_list[0] + '\n')
+            for k in range(len(nests)):
+                b.write('    nests[' + str(k) + '] = ' + str(nests[k]) + '\n')
+
+        
+            #print 'Main: (Before) ' + str(settings[0])
+            while True:
+                f = open(save_dir + 'best/' + save_as + '_best','a')
+                f.write(str(settings[0]) + ' / ' + str(nests[0][:6]) + ' / ' +  str(nests[0][6]) + '\n')
+                f.close()
+                nests = Cowbird.cuckoo_search(nests, iterations, 32, settings)
+
+                #Printing nests list into file
+                b = open(save_dir + 'current_nest_list.txt', 'w')
+                b.write('')
+                b = open(save_dir + 'current_nest_list.txt', 'a')
+                b.write('Time: ' + str(settings[0]) + '\n')
+                for k in range(len(nests)):
+                    b.write('    nests[' + str(k) + '] = ' + str(nests[k]) + '\n')
+
+        if optimization_type == 2:
+            print 'optimization 2'
+
+
+    #multi range of dates
+
+    if date_type == 2:
+
+        #Run if Cowbird chosen as the preferred algorithm
+        if optimization_type == 1:
+
+             #Run Grasshopper for all listed dates
+             for i in range(len(date_list)):
+                 print i, 'at the beginning'
+                 end_list[i], 'at the beginning'
+
+                 #For date in date_list: 
+                 #Appends the date to the end of original save_as name 
+                 save_as = original_save_as + str(date_list[i]) 
+
+                 #Changes the settings passed to get_score to only include a 
+                 #single date 
+                 settings = [date_list[i], base_dir, save_dir, save_as] 
+
+                 #Runs desired optimization 
+                 print '\n'*3 + 'Input date list: ' + str(date_list)
+
+                 #Begin timing initialization and add to vector "timing"
+                 timing.append(time.time())
        
-               #Initialize the 32 parameter sets
-	       nests = Cowbird.initialize(iterations, solution_number, settings)
+                 #Initialize the 32 parameter sets
+	         nests = Cowbird.initialize(iterations, solution_number, settings)
 
-               #End timing initialization and add to vector "timing"
-               timing.append(time.time())
+                 #Printing nests list into file
+                 b = open(save_dir + 'current_nest_list.txt', 'w')
+                 b.write('')
+                 b = open(save_dir + 'current_nest_list.txt', 'a')
+                 b.write('Time: ' + str(date_list[i]) + '\n')
+                 for k in range(len(nests)):
+                     b.write('    nests[' + str(k) + '] = ' + str(nests[k]) + '\n')
 
-               #Take the begin and end times out of the "timing" vector to 
-               #more easily be sent to the tuning run code
-               firsttime = timing[-2]
-               secondtime = timing[-1]
+                 #End timing initialization and add to vector "timing"
+                 timing.append(time.time())
+
+                 #Take the begin and end times out of the "timing" vector to 
+                 #more easily be sent to the tuning run code
+                 firsttime = timing[-2]
+                 secondtime = timing[-1]
 
                
-               tempdate = settings[0]
-               print i, 'before while'
+                 tempdate = settings[0]
+                 print i, 'before while'
+               
+                 #While the date is before the specified end time, run the program
+                 while int(tempdate)<int(end_list[i]): 
+                     begintime = settings[0]
+                     print 'inside tempdate loop'
+                     num = 0
+                     while num < iterations:
+                         print 'inside iter loop'
+                         f = open(save_dir + 'best/' + save_as + '_best','a')
+                         f.write(str(settings[0]) + ' / ' + str(nests[0][:6]) + ' / ' +  str(nests[0][6]) + '\n')
+                         f.close()
+                         total = 0
+                         for k in range(len(nests)):
+                            total = total + nests[k][6]
+                            avg = total/len(nests)
+                         g = open(save_dir + save_as + '_average','a')
+                         g.write(str(settings[0]) + ' / Average: ' +  str(avg) + '\n')
+                         g.close()
+
+
+                         print i, 'inside while'
+                         print end_list[i], 'end_list inside '
+                         print 'Main: (Before) ' + str(settings[0])
+
+                         #Run Cowbird for one tuning run
+	                 nests = Cowbird.cowbird_search(nests, iterations, solution_number, settings, firsttime, secondtime)        
+                         #Printing nests list into file
+                         b = open(save_dir + 'current_nest_list.txt', 'w')
+                         b.write('')
+                         b = open(save_dir + 'current_nest_list.txt', 'a')
+                         b.write('Time: ' + str(settings[0]) + '\n')
+                         for k in range(len(nests)):
+                             b.write('nests[' + str(k) + '] = ' + str(nests[k]) + '\n')
+
+                         print 'Main: (After) ' + str(settings[0])
+                         print i, 'at the end'
+                         print settings[0]
+                         print end_list[i]
                     
-                    #While the date is before the specified end time, run the program
-               while int(tempdate)<int(end_list[i]): 
-                    f = open(save_dir + 'best/' + save_as + '_best','a')
-                    f.write(str(settings[0]) + ' / ' + str(nests[0][:6]) + ' / ' +  str(nests[0][6]) + '\n')
-                    f.close()
+                         if iterations == 1:
 
-                    g = open(save_dir + save_as + '_average','a')
+                             #Update the temporary time
+                             tempdate = settings[0]
                     
-                    g.write(str(settings[0]) + ' / Average: ' +  sum(nests[:][6])/float(len(nests[:][6])) + '\n')
-                    g.close()
+                             print tempdate, 'tempdate'
 
-                    print i, 'inside while'
-                    print end_list[i], 'end_list inside '
-                    print 'Main: (Before) ' + str(settings[0])
+                         else:
+                             endtime = begintime + (settings[0]-begintime)
+                             tempdate = begintime
+                             settings[0] = begintime
 
-                         #Run Grasshopper for one tuning run
-	            nests = Cowbird.cowbird_search(nests, iterations, solution_number, settings, firsttime, secondtime)        
-                    print 'Main: (After) ' + str(settings[0])
-                    print i, 'at the end'
-                    print settings[0]
-                    print end_list[i]
+                             if (num+1 == iterations):
+                                 tempdate = endtime
 
-                         #Update the temporary time
-                    tempdate = settings[0]
-                    print tempdate, 'tempdate'
-                    
+                         num = num + 1
 
-
-    if optimization_type == 2:
-          print 'optimization 2'
+        if optimization_type == 2:
+            print 'optimization 2'
 
 
